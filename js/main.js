8399c51e746f3f86962ef4ae7145929aabcf04bc
fenglish <asuka.ochi@ft.com>
2017-08-25 12:45:56 +0100

    change nodeList to array  🐿 v2.5.16

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const Newsletter = require('./newsletter');
 
 module.exports = {
 	init: (containerEl = document.body) => {
-		const newsletterSignupContainers = containerEl.querySelectorAll('[data-component="n-newsletter-signup"]');
+		const newsletterSignupContainers = Array.from(containerEl.querySelectorAll('[data-component="n-newsletter-signup"]'));
 		if (newsletterSignupContainers.length) {
 			newsletterSignupContainers
 			.forEach(instanceEl => {
