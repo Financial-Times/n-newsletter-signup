70c7a9a09e30e3803d36f7199b3df0e8120ed69f
Nazehs Abel <talentednazeh@gmail.com>
2024-02-19 16:32:49 +0000

    chore(demo): Update demos to include the card state with image

--- demos/app.js
+++ demos/app.js
@@ -49,10 +49,12 @@ app.engine('.html', new PageKitHandlebars({
 app.use('/public', express.static(path.join(__dirname, '../public'), { redirect: false }));
 
 app.get('/', (req, res) => {
+	const state = req.query.state || 'default';
+	const cardState = fixtures[state];
 	res.render('demo', Object.assign({
 		title: 'Test App',
 		layout: 'demo-layout',
-	}, fixtures));
+	}, cardState));
 });
 
 app.post('/__myft/api/alerts/a0000000-a0a0-0000-a000-a000a0000a00/newsletters/000000000000000000000000/subscribe', (req, res) => {
